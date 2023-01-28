4cf35fc6773112f4b8c86989c8d3805422bc673f
brandonbate <brandonbate@gmail.com>
2023-01-28 15:49:13 -0500

    URLS for forms x2, moved in views + tests, new URL

--- lists/views.py
+++ lists/views.py
@@ -5,15 +5,12 @@ from lists.models import Item
 
 # Create your views here
 def home_page(request):
-    # Handles POST
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
-
-    # Handles GET
     return render(request, 'home.html')
 
 def view_list(request):
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
-    
+
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
