f120b7c2b1049e688994fe63ca0228dc23dd0486
brandonbate <brandonbate@gmail.com>
2023-01-28 16:53:38 -0500

    3 changed files, plus a migration (or two)

--- lists/models.py
+++ lists/models.py
@@ -3,5 +3,9 @@ from django.db import models
 # Create your models here.
 # A model represents the data in an application.
 
+class List(models.Model):
+    pass
+
 class Item(models.Model):
     text = models.TextField(default='')
+    list = models.ForeignKey(List, default=None)
