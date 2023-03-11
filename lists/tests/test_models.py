a8f69f9b295867005cc2ad24d9bf3a3d54b1580c
brandonbate <brandonbate@gmail.com>
2023-03-11 14:47:55 -0500

    Use get_absolute_url on list model to DRY urls in views

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -43,3 +43,11 @@ class ListAndItemModelsTest(TestCase):
         with self.assertRaises(ValidationError):
             item.save() #SQLite fails to tells us TextField is blank.
             item.full_clean()
+
+    def test_get_absolute_url(self):
+        list_ = List.objects.create()
+        self.assertEqual(list_.get_absolute_url(), f'/lists/{list_.id}/')
+
+
+
+
