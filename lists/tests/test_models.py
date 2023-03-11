635bf0a13365049e5959a4b36841d795b538361d
brandonbate <brandonbate@gmail.com>
2023-03-11 13:55:15 -0500

    Adjust new list views to do model validation

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.urls import resolve
 # TestCase is a modification of unittest.TestCase from earlier.
 from django.test import TestCase
@@ -36,4 +37,9 @@ class ListAndItemModelsTest(TestCase):
         self.assertEqual(second_saved_item.text, 'Item the second')
         self.assertEqual(second_saved_item.list, list_)
     
-      
+    def test_cannot_equal_empty_list(self):
+        list_ = List.objects.create()
+        item = Item(list=list_, text='')
+        with self.assertRaises(ValidationError):
+            item.save() #SQLite fails to tells us TextField is blank.
+            item.full_clean()
