cc6dd646542ce9066ce52903296f6fce700c7f46
brandonbate <brandonbate@gmail.com>
2023-03-18 13:58:46 -0400

    End of Chapter 15

--- lists/forms.py
+++ lists/forms.py
@@ -1,10 +1,17 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 from lists.models import Item
 
 EMPTY_ITEM_ERROR = "You can't have an empty list item"
+DUPLICATE_ITEM_ERROR = "You've already got this in your list"
 
+# Use this form for new lists because duplicates are impossible.
 class ItemForm(forms.models.ModelForm):
+
+    def save(self, for_list):
+        self.instance.list = for_list
+        return super().save()
 
     class Meta:
         model = Item
@@ -15,3 +22,23 @@ class ItemForm(forms.models.ModelForm):
                         )
         }
         error_messages = {'text': {'required': EMPTY_ITEM_ERROR}}
+
+# Duplicates can only happen with existings lists.
+class ExistingListItemForm(ItemForm):
+
+    # We add the for_list argument to the constructor.
+    def __init__(self, for_list, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.instance.list = for_list
+    
+    def save(self):
+        return forms.models.ModelForm.save(self)
+    
+    # This is called whenever we use the method form.is_valid()
+    def validate_unique(self):
+        try:
+            self.instance.validate_unique()
+        except ValidationError as e:
+            e.error_dict = {'text': [DUPLICATE_ITEM_ERROR]}
+            self._update_errors(e)
+
--- lists/views.py
+++ lists/views.py
@@ -2,7 +2,7 @@ from django.core.exceptions import ValidationError
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
 
-from lists.forms import ItemForm
+from lists.forms import ItemForm, ExistingListItemForm
 from lists.models import Item, List
 
 # Create your views here
@@ -11,28 +11,23 @@ def home_page(request):
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
-    error = None
+    form = ExistingListItemForm(for_list=list_)
     
     if request.method == 'POST':
-        try:
-            item = Item(text=request.POST['item_text'], list=list_)
-            item.full_clean()
-            item.save()
+        form = ExistingListItemForm(for_list=list_, data=request.POST)
+        if form.is_valid():
+            form.save()
             return redirect(list_)
-        except:
-            error = "You can't have an empty list item"
-            
-    return render(request, 'list.html', {'list': list_, 'error': error})
+          
+    return render(request, 'list.html', {'list': list_, 'form': form})
     
 def new_list(request):
-    list_ = List.objects.create()
-    item = Item(text=request.POST['item_text'], list=list_)
-    try:
-        item.full_clean()
-        item.save()
-    except ValidationError:
-        list_.delete()
-        error = "You can't have an empty list item"
-        return render(request, 'home.html', {"error": error})
+    form = ItemForm(data=request.POST)
+    if form.is_valid():
+        list_ = List.objects.create()
+        form.save(for_list=list_)
+        return redirect(list_)
+    else:
+        return render(request, 'home.html', {'form': form})
         
-    return redirect(list_)
+ 
